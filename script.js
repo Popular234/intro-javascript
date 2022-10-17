96ae72c155e3236df5ecb94d86abfef32cb4c6c9
Popular234 <cainjose24@g.crookcountyschools.org>
2022-10-17 13:58:12 -0700

    added some javascript and made the prompts and alerts

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("hello world");
+
+let number1= 16;
+
+let number2= 3;
+
+function math(number1, number2){
+    let equaltoo = number1 / number2
+    console.log("here are 2 numbers " + number1 + " and " + number2 + " if we divide them we get " + equaltoo)
+} 
+
+const String = ("this function was made with a function")
+
+document.write("<h1> this is all done within javascript</h1>")
+
+math(16, 3)
+console.log(String)
+prompt("you ready to have your mind blown?")
+alert("press OK if you're sure")
